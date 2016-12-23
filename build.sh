4af9526cfc8d01181e7eb4fe1b70df463c018395
Lukasz Wartalski <lukasz@gamehouse.com>
2016-12-23 16:24:34 +0100

    update build script to handle errors properly

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-mvn -f config-server/pom.xml clean package &
-mvn -f twitter-service/pom.xml clean package &
-mvn -f webapp/pom.xml clean package &
-wait
-docker-compose build &
-wait
+mvn -f config-server/pom.xml clean package &&
+mvn -f twitter-service/pom.xml clean package &&
+mvn -f webapp/pom.xml clean package &&
+docker-compose build
