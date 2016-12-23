97458efcbcf52c32333c12b67de119d40d57cbad
Lukasz Wartalski <lukasz@gamehouse.com>
2016-12-23 21:29:02 +0100

    Run project using images from docker hub or by building images from source

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 cd config-server/ && ./mvnw clean package && cd .. &&
 cd twitter-service/ && ./mvnw clean package && cd .. &&
 cd webapp/ && ./mvnw clean package && cd .. &&
-docker-compose build
+docker-compose -f docker-compose-local-images.yml build
